120d23ada27c31e101b6dbf007306211f3c04f58
HarrisForeman <harris.foreman@gmail.com>
2017-04-26 15:44:05 +1200

    Memes

---
+++ Game.js
@@ -0,0 +1,43 @@
+var char_x = 0;
+var char_y = 0;
+
+    document.getElementById('right').addEventListener('click', function() {
+
+      if( char_x < 750){
+        char_x+= 50;
+        document.getElementById('char').style.left = char_x+'px';
+      }
+
+    });
+
+
+
+    document.getElementById('left').addEventListener('click', function() {
+
+      if( char_x > 0){
+        char_x-= 50;
+        document.getElementById('char').style.left = char_x+'px';
+      }
+
+    });
+
+
+
+    document.getElementById('down').addEventListener('click', function() {
+
+      if( char_y < 550){
+        char_y+= 50;
+        document.getElementById('char').style.top = char_y+'px';
+      }
+
+    });
+
+
+    document.getElementById('up').addEventListener('click', function() {
+
+      if( char_y > 0){
+        char_y -= 50;
+        document.getElementById('char').style.top = char_y+'px';
+      }
+
+    });
